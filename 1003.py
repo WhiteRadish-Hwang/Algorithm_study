e9de879c52f6eada733a4c927e096c31e962619a
SY Hwang <hain1218@gmail.com>
2020-08-28 23:44:43 +0900

    백준 1003 업로드

---
+++ 1003.py
@@ -0,0 +1,29 @@
+t = int(input())
+n = []
+for i in range(t):
+    n.append(int(input()))
+cnt0 = 0
+cnt1 = 0
+def fibonachi(n):
+    global cnt0
+    global cnt1
+    if n == 0:
+        dp[n] = 0
+        cnt0 += 1
+        return dp[n]
+    if n == 1:
+        dp[n] = 1
+        cnt1 += 1
+        return dp[n]
+    
+    if dp[n] != -1:
+        return dp[n]
+    dp[n] = fibonachi(n-1) + fibonachi(n-2)
+    return dp[n]
+
+for i in range(t):
+    dp = [-1]*(n[i]+1)
+    fibonachi(n[i])
+    print(f"{cnt0} {cnt1}")
+    cnt0 = 0
+    cnt1 = 0
